96fee43d7faaf0956d590a49c4efd09021cd20d8
Evan Wallace <evan.exe@gmail.com>
2020-07-09 17:04:53 -0700

    add a plugin api

--- internal/bundler/bundler_default_test.go
+++ internal/bundler/bundler_default_test.go
@@ -1386,7 +1386,7 @@ func TestRequireBadExtension(t *testing.T) {
 			IsBundling:    true,
 			AbsOutputFile: "/out.js",
 		},
-		expectedScanLog: `/entry.js: error: File extension not supported: /test
+		expectedScanLog: `/entry.js: error: File could not be loaded: /test
 `,
 	})
 }
